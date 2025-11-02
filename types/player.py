660572e1f11ec5b1046d5c4a81ac2e589b39992a
SnowyAhh <sylviahuynh17@gmail.com>
2025-11-02 16:32:26 +1100

    Adjusted player attack to be 20 for faster battles

--- types/player.py
+++ types/player.py
@@ -4,6 +4,6 @@ class Player(Entity):
     def __init__(self) -> None:
         # Set starting stats
         starting_health = 100
-        starting_attack = 10
+        starting_attack = 20
         player_name = "You"
         super().__init__(starting_health, starting_attack, player_name)
