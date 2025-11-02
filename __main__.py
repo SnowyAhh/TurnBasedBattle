c0433e4155e0ba748ce25aea8119c1d4481ae1f2
SnowyAhh <sylviahuynh17@gmail.com>
2025-11-02 16:31:51 +1100

    Added main file

---
+++ __main__.py
@@ -0,0 +1,10 @@
+from types.player import Player
+from battle import Battle
+# File for getting terminal input and showing terminal output
+def main():
+    player = Player()
+    battle = Battle(player)
+    battle.battle()
+
+if __name__ == "__main__":
+    main()
