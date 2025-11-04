45c4bc068e93e30adc8c4599d36f0480e1344f2d
SnowyAhh <sylviahuynh17@gmail.com>
2025-11-04 20:46:29 +1100

    Added pre-populated list of items

---
+++ battle_types/initalise_items.py
@@ -0,0 +1,7 @@
+from battle_types.item import *
+
+# Initalise items
+item_list = {
+    "sml_hp_potion": Item("Small health potion", ItemTypes.HEALTH, 15, "Heals 15 hp"),
+    "med_hp_potion": Item("Medium health potion", ItemTypes.HEALTH, 30, "Heals 30 hp")
+}
