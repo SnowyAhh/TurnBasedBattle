a98d415182f5c1983f7dfa516e00ff39a7bdd463
SnowyAhh <sylviahuynh17@gmail.com>
2025-11-04 20:46:04 +1100

    Added item class

---
+++ battle_types/item.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ItemTypes(Enum):
+    HEALTH = 1
+
+class Item:
+    def __init__(self, name, type, points, description) -> None:
+        self.name = name
+        self.type = type
+        self.points = points # How much it buffs
+        self.description = description
+        
