4639762a2b3a00db8a9e32685d16993da236f31e
SnowyAhh <sylviahuynh17@gmail.com>
2025-11-13 10:29:07 +1100

    Added crit rate and crit damage items

--- battle_types/initalise_items.py
+++ battle_types/initalise_items.py
@@ -4,7 +4,14 @@ from battle_types.item import *
 item_list = {
     "sml_hp_potion": Item("Small health potion", ItemTypes.HEALTH, 15, "Heals 15 hp"),
     "med_hp_potion": Item("Medium health potion", ItemTypes.HEALTH, 30, "Heals 30 hp"),
+    "lrg_atk_potion": Item("Large health potion", ItemTypes.HEALTH, 50, "Heals 50 hp"),
     "sml_atk_potion": Item("Small strength potion", ItemTypes.ATTACK, 5, "Increases attack by 5"),
     "med_atk_potion": Item("Medium strength potion", ItemTypes.ATTACK, 10, "Increases attack by 10"),
     "lrg_atk_potion": Item("Large strength potion", ItemTypes.ATTACK, 20, "Increases attack by 20"),
+    "sml_rate_potion": Item("Small crit rate potion", ItemTypes.CRIT_RATE, 0.1, "Increases crit rate by 0.1"),
+    "med_rate_potion": Item("Medium crit rate potion", ItemTypes.CRIT_RATE, 0.2, "Increases crit rate by 0.2"),
+    "lrg_rate_potion": Item("Large crit rate potion", ItemTypes.CRIT_RATE, 0.3, "Increases crit rate by 0.3"),
+    "sml_cdmg_potion": Item("Small crit damage potion", ItemTypes.CRIT_DAMAGE, 0.1, "Increases crit damage by 0.1"),
+    "med_cdmg_potion": Item("Medium crit damage potion", ItemTypes.CRIT_DAMAGE, 0.40, "Increases crit rate by 0.40"),
+    "lrg_cdmg_potion": Item("Large crit damage potion", ItemTypes.CRIT_DAMAGE, 1.0, "Increases crit rate by 1.0"),
 }
--- battle_types/item.py
+++ battle_types/item.py
@@ -2,10 +2,13 @@ from enum import Enum
 
 class ItemTypes(Enum):
     HEALTH = 1,
-    ATTACK = 2
+    ATTACK = 2,
+    CRIT_RATE = 3,
+    CRIT_DAMAGE = 4
 
 class Item:
-    def __init__(self, name, type, points, description) -> None:
+    def __init__(self, name: str, type: ItemTypes, points: float | int, 
+                 description: str) -> None:
         self.name = name
         self.type = type
         self.points = points # How much it buffs
